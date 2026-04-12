5ddf71d26a31038c44bf66ffc92d85890dbec013
SerialCore <serialcore@outlook.com>
2026-04-12 18:38:47 +0800

    back to the version before AI modified

--- src/model/radius.c
+++ src/model/radius.c
@@ -30,19 +30,19 @@ void radius_meson_rms(const argsInput_t *input, const matrix_t *vector, array_t 
         basis[i].scale = getnu(i + 1, nmax, rmax, rmin);
     }
 
-    /* construct matrices */
-    matrix_t mR2 = matrix_init(nmax, nmax);
-    matrix_t mOver = matrix_init(nmax, nmax);
-
     /* prepare variables */
     argsOrbit_t args_bra;
     argsOrbit_t args_ket;
     double factor;
+    double coef;
     double r2sum;
     double oversum;
     double fm = 5.06773093854369882649;
 
-    /* precompute matrices */
+    /* construct matrices */
+    matrix_t mR2 = matrix_init(nmax, nmax);
+    matrix_t mOver = matrix_init(nmax, nmax);
+
     for (int i = 0; i < nmax; i++) {
         for (int j = 0; j < nmax; j++) {
             args_bra = basis[i];
@@ -54,57 +54,19 @@ void radius_meson_rms(const argsInput_t *input, const matrix_t *vector, array_t 
         }
     }
 
-    /* Cholesky decomposition, construct matrix R' of r^2 under orthogonal basis */
-    matrix_t Lm = matrix_init(nmax, nmax);
-    matrix_t Linv = matrix_init(nmax, nmax);
-    matrix_cholesky_decomp(&mOver, &Lm);
-    matrix_inverse_lowertri(&Lm, &Linv);
-
-    matrix_t temp = matrix_init(nmax, nmax);
-    matrix_t LinvT = matrix_init(nmax, nmax);
-    matrix_t Rprime = matrix_init(nmax, nmax);
-    matrix_product(&Linv, &mR2, &temp);             /* Linv * R */
-    matrix_transpose(&Linv, &LinvT);                /* Linv^T = L^{-T} */
-    matrix_product(&temp, &LinvT, &Rprime);         /* R' = Linv * R * Linv^T */
-
-    /* Free temporary matrices after transformation (before loop) */
-    matrix_free(&Linv);
-    matrix_free(&temp);
-    matrix_free(&LinvT);
-
-    /* calculate rms radius with orthogonal coefficients d */
+    /* calculate RMS radius */
     for (int n = 0; n < len; n++) {
-        double *d_vec = (double *)calloc(nmax, sizeof(double));
         r2sum = 0.0;
         oversum = 0.0;
-
-        /* d = L^T c */
         for (int i = 0; i < nmax; i++) {
             for (int j = 0; j < nmax; j++) {
-                d_vec[i] += Lm.value[j][i] * vector->value[n][j];   /* L^T[i][j] = L[j][i] */
-            }
-            oversum += d_vec[i] * d_vec[i];
-        }
-
-        /* r2sum = d^T R' d */
-        for (int i = 0; i < nmax; i++) {
-            for (int j = 0; j < nmax; j++) {
-                r2sum += d_vec[i] * Rprime.value[i][j] * d_vec[j];
+                coef = vector->value[n][i] * vector->value[n][j];
+                r2sum += coef * mR2.value[i][j];
+                oversum += coef * mOver.value[i][j];
             }
         }
-
-        radius->value[n] = (oversum > 1e-12) ? sqrt(r2sum / oversum) / fm : 0.0;
-
-        double maxd = 0.0;
-        for (int i = 0; i < nmax; i++) if (fabs(d_vec[i]) > maxd) maxd = fabs(d_vec[i]);
-        printf("State %2d: RMS=%.6f  max|d|=%.3f  oversum=%.12f\n", n+1, radius->value[n], maxd, oversum);
-
-        free(d_vec);
+        radius->value[n] = sqrt(r2sum / oversum) / fm;
     }
-
-    /* Free matrices after loop completes */
-    matrix_free(&Lm);
-    matrix_free(&Rprime);
 
     free(basis);
     matrix_free(&mR2);
